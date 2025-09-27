fc61e0ffdb4a04ef754d3fa0a8928e968849ba2f
Augusto Arlt <augustoarlt@Augustos-MacBook-Pro.local>
2025-09-27 16:45:34 -0300

    fix(compiler): add missing type

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,13 @@ import { HomeScreen } from "@/components/screens/Home";
 import { LoadingSkeletons } from "@/components/screens/Home/components/LoadingSkeletons";
 import { Suspense } from "react";
 
-export default async function Home({ searchParams }) {
+type HomeProps = {
+  searchParams: {
+    genre?: string;
+  };
+};
+
+export default async function Home({ searchParams }: HomeProps) {
   const genre = searchParams.genre;
   return (
     <main className="flex min-h-screen flex-col items-center justify-between px-6 py-8">
