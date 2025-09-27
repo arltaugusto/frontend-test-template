22a1e5b4dca4f10163b1a29150aa522111e03c77
Augusto Arlt <augustoarlt@Augustos-MacBook-Pro.local>
2025-09-27 16:37:25 -0300

    feat: add game service

---
+++ src/services/game.ts
@@ -0,0 +1,26 @@
+import { allGames, delay } from "@/utils/endpoint";
+
+const ITEMS_PER_PAGE = 12;
+async function getGamesByPageAndGenre(page: number, genre: string | null) {
+  let games = allGames;
+
+  if (genre) {
+    games = games.filter((game) => game.genre.toLowerCase() === genre.toLowerCase());
+  }
+
+  if (page < 1 || isNaN(page)) page = 1;
+
+  // Mock a delay to simulate a real API
+  await delay(2000);
+
+  const fromIndex = (page - 1) * ITEMS_PER_PAGE;
+  const toIndex = page * ITEMS_PER_PAGE;
+  games = games.slice(fromIndex, toIndex);
+
+  const totalPages = Math.ceil(allGames.length / ITEMS_PER_PAGE);
+  const currentPage = page;
+
+  return { totalPages, currentPage, games };
+}
+
+export { getGamesByPageAndGenre };
