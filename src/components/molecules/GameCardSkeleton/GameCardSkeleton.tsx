442c8098a4b47693b33f616d8d8c1aac0cf2eca7
Augusto Arlt <augustoarlt@Augustos-MacBook-Pro.local>
2025-09-27 02:26:26 -0300

    feat(element): Add GameCardSkeleton

---
+++ src/components/molecules/GameCardSkeleton/GameCardSkeleton.tsx
@@ -0,0 +1,18 @@
+const GameCardSkeleton = () => {
+  return (
+    <div className="flex animate-pulse flex-col gap-y-5 rounded-2xl border border-neutral-750 p-6">
+      <div className="h-[218px] w-[279px] rounded-t-2xl bg-gray-300" />
+      <div className="flex flex-col gap-y-3">
+        <div className="h-5 w-16 rounded bg-gray-300" />
+        <div className="flex justify-between">
+          <div className="h-5 w-28 rounded bg-gray-300" />
+          <div className="h-5 w-12 rounded bg-gray-300" />
+        </div>
+      </div>
+
+      <div className="h-[56px] w-full rounded-lg bg-gray-300" />
+    </div>
+  );
+};
+
+export { GameCardSkeleton };
