b2b4ba6c0b4df9bd03c636d06f18d29c880ce908
Augusto Arlt <augustoarlt@Augustos-MacBook-Pro.local>
2025-09-27 02:03:35 -0300

    style: add new colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,15 +9,17 @@ const config: Config = {
   theme: {
     extend: {
       backgroundColor: {
-        "neutral-550": "#EEEEEE",
+        "neutral-650": "#585660",
       },
       colors: {
+        "neutral-550": "#EEEEEE",
         "neutral-650": "#585660",
+        "neutral-750": "#8F8F8F",
+        "neutral-850": "#3B3B3B",
       },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
