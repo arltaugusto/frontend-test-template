ac03f2e910291d7d5b250d59c2931509bb6d08fc
Augusto Arlt <augustoarlt@Augustos-MacBook-Pro.local>
2025-09-27 02:02:26 -0300

    feat(elements): Add button atom component

---
+++ src/components/atoms/Button/Button.tsx
@@ -0,0 +1,39 @@
+"use client";
+import { clsx } from "clsx";
+import React from "react";
+
+type Variant = "primary" | "secondary";
+interface ButtonProps extends React.ComponentPropsWithoutRef<"button"> {
+  onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  variant?: Variant;
+  className?: string;
+}
+
+const VARIANTS: Record<Variant, string> = {
+  primary: "border border-neutral-650 bg-neutral-650",
+  secondary: "border border-neutral-850 box-border text-neutral-850",
+};
+const Button = ({
+  children,
+  className,
+  onClick,
+  variant = "primary",
+  ...rest
+}: React.PropsWithChildren<ButtonProps>) => {
+  const buttonVariant = VARIANTS[variant];
+  return (
+    <button
+      onClick={onClick}
+      className={clsx(
+        "flex items-center justify-center rounded-lg py-5 text-base font-bold leading-4",
+        buttonVariant,
+        className,
+      )}
+      {...rest}
+    >
+      {children}
+    </button>
+  );
+};
+
+export { Button };
