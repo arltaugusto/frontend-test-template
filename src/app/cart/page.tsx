14bf3278b55b0afa9446b17179c9b685dda8a8f3
Augusto Arlt <augustoarlt@Augustos-MacBook-Pro.local>
2025-09-29 12:46:06 -0300

    fix: fix global styles for short body

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -2,7 +2,7 @@ import { CartScreen } from "@/components/screens/Cart";
 
 export default async function Cart() {
   return (
-    <main className="mt-4 flex flex-col px-6 2xl:mt-6 2xl:px-32">
+    <main className="mt-4 flex flex-1 flex-col px-6 2xl:mt-6 2xl:px-32">
       <CartScreen />
     </main>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default async function Home({ searchParams }: HomeProps) {
   const genre = searchParams.genre;
 
   return (
-    <main className="mt-8 flex flex-col 2xl:mt-12">
+    <main className="mt-8 flex flex-col 2xl:mt-12 flex-1">
       <TopSellers />
       <div className="px-6 py-8 2xl:px-32">
         <Suspense key={genre} fallback={<LoadingSkeletons />}>
