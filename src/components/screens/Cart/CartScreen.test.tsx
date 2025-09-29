ee970a8eba312aa4b7e434678bcaaa59d495d77f
Augusto Arlt <augustoarlt@Augustos-MacBook-Pro.local>
2025-09-29 08:37:37 -0300

    test: Add CartScreen tests

---
+++ src/components/screens/Cart/CartScreen.test.tsx
@@ -0,0 +1,47 @@
+import { render, screen } from "@testing-library/react";
+
+import { allGames } from "@/utils/endpoint";
+import { useCartContext } from "@/contexts/CartContext";
+import { CartScreen } from ".";
+
+const mockGame = allGames[0];
+jest.mock("@/contexts/CartContext", () => ({
+  useCartContext: jest.fn(),
+}));
+
+jest.mock("./components/GamesInCart", () => ({
+  GamesInCart: () => <div>GamesInCart</div>,
+}));
+
+jest.mock("./components/Checkout", () => ({
+  Checkout: () => <div>Checkout</div>,
+}));
+
+describe("CartScreen", () => {
+  it("should render checkout and cart if there are items in cart", () => {
+    jest.mocked(useCartContext).mockReturnValue({
+      cart: { [mockGame.id]: mockGame },
+      addItem: jest.fn(),
+      removeItem: jest.fn(),
+    });
+
+    render(<CartScreen />);
+
+    expect(screen.getByText("GamesInCart")).toBeInTheDocument();
+    expect(screen.getByText("Checkout")).toBeInTheDocument();
+    expect(screen.getByText("1 item")).toBeInTheDocument();
+  });
+
+  it("should not render checkout and cart if cart is empty", () => {
+    jest.mocked(useCartContext).mockReturnValue({
+      cart: {},
+      addItem: jest.fn(),
+      removeItem: jest.fn(),
+    });
+
+    render(<CartScreen />);
+    expect(screen.queryByText("GamesInCart")).not.toBeInTheDocument();
+    expect(screen.queryByText("Checkout")).not.toBeInTheDocument();
+    expect(screen.getByText("0 items")).toBeInTheDocument();
+  });
+});
