33306610b71917b4a07018b5a6b0a0fbb566caa4
Augusto Arlt <augustoarlt@Augustos-MacBook-Pro.local>
2025-09-29 08:37:25 -0300

    test: jest and rtl setup

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,27 @@
-import type { Config } from "jest";
+import type { Config } from "@jest/types";
+import nextJest from "next/jest.js";
 
-const config: Config = {
+export const customJestConfig: Config.InitialOptions = {
+  coverageProvider: "v8",
+  clearMocks: true,
+  setupFilesAfterEnv: ["<rootDir>/setupJest.ts"], // Adjust path if needed
+  testEnvironment: "jest-environment-jsdom",
   verbose: true,
 };
 
-export default config;
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+const jestConfig = async () => {
+  const nextJestConfig = await createJestConfig(customJestConfig)();
+  return {
+    ...nextJestConfig,
+    moduleNameMapper: {
+      "\\.svg$": "<rootDir>/__mocks__/svg.tsx",
+      "^@/(.*)$": "<rootDir>/src/$1",
+      ...nextJestConfig.moduleNameMapper,
+    },
+  };
+};
+export default jestConfig;
