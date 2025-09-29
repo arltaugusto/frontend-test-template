2ac764f3fe939cabc9e66a09b5886b121adfb0fb
Augusto Arlt <augustoarlt@Augustos-MacBook-Pro.local>
2025-09-29 11:27:23 -0300

    test: fix svgs mocks

--- __mocks__/svg.tsx
+++ __mocks__/svg.tsx
@@ -1,2 +1,9 @@
-export default "SvgrURL";
-export const ReactComponent = "div";
+import React from "react";
+
+const SvgrMock = React.forwardRef((props, ref) => (
+  // @ts-ignore
+  <span ref={ref} {...props} data-testid="svg-mock" />
+));
+
+export const ReactComponent = SvgrMock;
+export default SvgrMock;
