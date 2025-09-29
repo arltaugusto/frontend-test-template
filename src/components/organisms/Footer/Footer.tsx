432be845c5636fde88105b85e6aa47246cb2cea4
Augusto Arlt <augustoarlt@Augustos-MacBook-Pro.local>
2025-09-29 12:32:38 -0300

    fix: Add missing homepage link

--- src/components/organisms/Footer/Footer.tsx
+++ src/components/organisms/Footer/Footer.tsx
@@ -1,9 +1,13 @@
+import Link from "next/link";
+
 import ApplyDigitalLogo from "./assets/apply-digital-logo.svg";
 
 const Footer = () => {
   return (
     <footer className="mt-8 flex items-center justify-center bg-neutral-700 py-16 2xl:mt-12">
-      <ApplyDigitalLogo />
+      <Link href="/">
+        <ApplyDigitalLogo />
+      </Link>
     </footer>
   );
 };
