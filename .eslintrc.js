a2f82805cdf524a0847e6321a97c78d895f0748e
Augusto Arlt <augustoarlt@Augustos-MacBook-Pro.local>
2025-09-29 11:42:01 -0300

    fix: eslint typescript rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,7 @@
+const { rules } = require("eslint-plugin-react");
+
 module.exports = {
+  parser: "@typescript-eslint/parser",
   extends: [
     "next",
     "next/core-web-vitals",
@@ -6,9 +9,23 @@ module.exports = {
     "plugin:react/recommended",
     "prettier",
   ],
-  plugins: ["react"],
+  overrides: [
+    {
+      files: ["**/*.test.tsx", "**/*.test.ts"],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+  plugins: ["react", "@typescript-eslint"],
   parserOptions: {
     ecmaVersion: 2021,
     sourceType: "module",
+  },
+  rules: {
+    ...rules.rules,
+    "no-unused-vars": 0,
+    "@typescript-eslint/no-unused-vars": 2,
+    "react/react-in-jsx-scope": 0,
   },
 };
